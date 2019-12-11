2a09fea98b6bc08e2c0785ab6724cfb980805459
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2019-12-11 11:19:40 +0100

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float AmountOfHealth;//1 health is a half hearth
+
+    public float TakeDamage(float amount)
+    {
+        AmountOfHealth -= amount;
+        return AmountOfHealth;
+    }
+
+    public float GetHealth() { return AmountOfHealth; }
+}
